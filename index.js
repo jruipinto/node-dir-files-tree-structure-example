b9b8e3483d0b96c02ef6503c468f51e9546cbc5c
jruipinto <44852805+jruipinto@users.noreply.github.com>
2020-09-10 09:15:57 +0100

    refactor: convert dir.paths to promise

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ const path = './uploads';
 //     console.log(files);
 // });
 
-dir.paths(path, (err, paths) => {
-    if (err) throw err;
-    console.log('files:\n', paths.files);
-    console.log('subdirs:\n', paths.dirs);
-});
+// dir.paths(path, (err, paths) => {
+//     if (err) throw err;
+//     console.log('files:\n', paths.files);
+//     console.log('subdirs:\n', paths.dirs);
+// });
+
+
+const filesPromise = new Promise(
+    (resolve, reject) => {
+        dir.paths(path, (err, paths) => { err ? reject(err) : resolve(paths) });
+    }
+)
+    .then(({ files, dirs }) => {
+        return ({
+            files: files.map(file => file.replace(/^[\w\d\s]*\\\\/g, '')),
+            dirs: dirs.map(file => file.replace(/^[\w\d\s]*\\\\/g, ''))
+        });
+    })
+    ;
+
+
+(async () => {
+    console.log(await filesPromise);
+})()
