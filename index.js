3abb60457b11d8a37beef658ac2d75a11dd05be7
jruipinto <rui.jruipinto@gmail.com>
2020-09-12 08:14:41 +0000

    refactor: remove deps & add a function with code copied from 1 of the deps

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const getDirTreeFrom = require('directory-tree'); // sync
-const dirTree = require('directory-tree-promise'); // async
+const dirTree = require('./directory-tree-async');
 
 const path = './uploads';
 
-// console.log('dir tree:', getDirTreeFrom(path, { normalizePath: true }));
-
 (
-    async => {
-        console.log(
-            await dirTree(path)
-        );
-    }
+  async () => {
+    console.log(
+      await dirTree(path)
+    );
+  }
 )();
