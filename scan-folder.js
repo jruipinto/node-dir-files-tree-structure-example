ab0f94bd53ce56ff8a970be7bb2c452fdc532633
jruipinto <rui.jruipinto@gmail.com>
2020-09-21 12:52:15 +0000

    refactor: UNIX'ify Windows paths

--- scan-folder.js
+++ scan-folder.js
@@ -9,50 +9,50 @@ const readdir = promisify(fs.readdir)
 async function scanFolder(
   path = __dirname,
   options = { scanDeep: false }
-  ) {
+) {
 
-    const { scanDeep } = options
+  const { scanDeep } = options
 
-    try {
+  try {
 
-      const files = Promise.all(
-        await readdir(path).then(
-          filesNames => filesNames.map(
-            fileName => lstat(join(path, fileName))
-              .then(
-                fileProperties => ({
-                  name: fileName,
-                  path: join(path, fileName),
-                  accessedAt: fileProperties.atime,
-                  createdAt: fileProperties.birthtime,
-                  updatedAt: fileProperties.mtime,
-                  size: fileProperties.size,
-                  isFile: fileProperties.isFile(),
-                  isDirectory: fileProperties.isDirectory(),
-                  isSymbolicLink: fileProperties.isSymbolicLink(),
-                  extension: extname(fileName)
-                })
-              )
-              .then(
-                file => {
-                  if (file.isDirectory && scanDeep) {
-                    return scanFolder(file.path, scanDeep).then(
-                      tree => ({ ...file, children: tree })
-                    )
-                  }
-                  return file
+    const files = Promise.all(
+      await readdir(path).then(
+        filesNames => filesNames.map(
+          fileName => lstat(join(path, fileName))
+            .then(
+              fileProperties => ({
+                name: fileName,
+                path: join(path, fileName).replace(/\\/g, '/'),
+                accessedAt: fileProperties.atime,
+                createdAt: fileProperties.birthtime,
+                updatedAt: fileProperties.mtime,
+                size: fileProperties.size,
+                isFile: fileProperties.isFile(),
+                isDirectory: fileProperties.isDirectory(),
+                isSymbolicLink: fileProperties.isSymbolicLink(),
+                extension: extname(fileName)
+              })
+            )
+            .then(
+              file => {
+                if (file.isDirectory && scanDeep) {
+                  return scanFolder(file.path, { scanDeep }).then(
+                    tree => ({ ...file, children: tree })
+                  )
                 }
-              )
-          )
+                return file
+              }
+            )
         )
       )
+    )
 
-      return files
+    return files
 
-    } catch (error) {
-      console.log(error)
-      return {}
-    }
+  } catch (error) {
+    console.log(error)
+    return {}
+  }
 }
 
 module.exports = scanFolder
